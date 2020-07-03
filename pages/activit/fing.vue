54fbe51cc2063fdbb1b2e567995ee224748a9116
Kannathorn <tiwtiw197@gmail.com>
2020-07-02 22:49:45 -0700

    start

---
+++ pages/activit/fing.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-center">
+      <div>
+        กิจกรรมหน้าเสาธง
+    </div>
+    <div>
+      <v-btn class="ma-2" color="orange darken-2" dark>
+        <nuxt-link to="/activit/menu">
+        <v-icon dark left>mdi-arrow-left</v-icon>Back
+        </nuxt-link>
+      </v-btn>
+    </div>
+  </div>
+</template>
---
+++ pages/activit/menu.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-container fluid class="pa-0">
+    <v-row align="center">
+      <v-col cols="12" sm="6">
+        <div class="text-center">
+          <div class="my-2">
+            <nuxt-link to="/activit/fing">
+              <v-btn color="secondary" dark>กิจกรรมหน้าเสาธง</v-btn>
+            </nuxt-link>
+          </div>
+          <div class="my-2">
+            <v-btn color="primary" dark>กิจกรรมโฮมรูม</v-btn>
+          </div>
+          <div class="my-2">
+            <v-btn color="warning" dark>กิจกรรมพิเศษ</v-btn>
+          </div>
+          <div class="my-2">
+            <v-btn color="error" dark large>กิจกรรมหน้าเสาธง</v-btn>
+          </div>
+          <div class="my-2">
+            <v-btn color="success" dark>กิจกรรมหน้าเสาธง</v-btn>
+          </div>
+        </div>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <div class="text-center">
+          <div class="my-2">
+            <v-btn color="secondary" fab x-small dark>
+              <v-icon>mdi-television</v-icon>
+            </v-btn>
+          </div>
+          <div class="my-2">
+            <v-btn color="primary" fab small dark>
+              <v-icon>mdi-pencil</v-icon>
+            </v-btn>
+          </div>
+          <div class="my-2">
+            <v-btn color="warning" fab dark>
+              <v-icon>mdi-account-circle</v-icon>
+            </v-btn>
+          </div>
+          <div class="my-2">
+            <v-btn color="error" fab large dark>
+              <v-icon>mdi-alarm</v-icon>
+            </v-btn>
+          </div>
+          <div class="my-2">
+            <v-btn color="success" fab x-large dark>
+              <v-icon>mdi-domain</v-icon>
+            </v-btn>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
